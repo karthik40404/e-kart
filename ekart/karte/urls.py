db3b377f8288e2cc526a7c348ba35364a0f82643
karthik40404 <karpython123@gmail.com>
2024-11-15 11:22:48 +0530

    session

--- ekart/karte/urls.py
+++ ekart/karte/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('',views.log),
     path('shop_home',views.shop_home),
     path('register',views.reg),
-    path('logout',views.log_out)
+    path('logout',views.log_out),
+    path('add',views.addproduct)
 ]
--- ekart/karte/views.py
+++ ekart/karte/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .models import *
 
 # Create your views here.
 def log(req):
@@ -22,7 +23,8 @@ def log(req):
 
 def shop_home(req):
     if 'shop' in req.session:
-        return render(req,'shop/home.html')
+        data=Product.objects.all()
+        return render(req,'shop/home.html',{'products':data})
     else:
         return redirect(log)
 
@@ -31,8 +33,23 @@ def log_out(req):
     req.session.flush()
     return redirect(log)
 
-
-
+def addproduct(req):
+    if 'shop' in req.session:
+        if req.method=='POST':
+            pid=req.POST['pid']
+            name=req.POST['name']
+            disc=req.POST['disc']
+            price=req.POST['price']
+            offer_price=req.POST['offer_price']
+            stock=req.POST['stock']
+            file=req.FILES['img']
+            data=Product.objects.create(pid=pid,name=name,disc=disc,price=price,offer_price=offer_price,stock=stock,img=file)
+            data.save()
+            return redirect(shop_home)
+        else:
+            return render(req,'shop/addp.html')
+    else:
+        return redirect(log)
 
 
 
